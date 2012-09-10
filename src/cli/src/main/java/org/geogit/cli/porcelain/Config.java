915cce7759d700f6ae0c1e776d20913c5c7fb8cb
Michael Fawcett <michael.fawcett@lmnsolutions.com>
2012-09-10 14:31:59 -0400

    Formatted. Refactored and expanded error handling.

--- src/cli/src/main/java/org/geogit/cli/porcelain/Config.java
+++ src/cli/src/main/java/org/geogit/cli/porcelain/Config.java
@@ -24,56 +24,58 @@ import com.beust.jcommander.Parameters;
 @Parameters(commandNames = "config", commandDescription = "Get and set repository or global options")
 public class Config extends AbstractCommand implements CLICommand {
 
-    @Parameter(names = "--global", description = "For writing options: write to global ~/.geogitconfig file rather than the repository .geogit/config." +
-    "For reading options: read only from global ~/.geogitconfig rather than from all available files.")
+    @Parameter(names = "--global", description = "For writing options: write to global ~/.geogitconfig file rather than the repository .geogit/config."
+            + "For reading options: read only from global ~/.geogitconfig rather than from all available files.")
     private boolean global = false;
-	
+
     @Parameter(names = "--get", description = "Get the value for a given key. Returns error code 1 if the key was not found and error code 2 if multiple key values were found.")
     private boolean get = false;
-    
+
     @Parameter(description = "name value")
     private List<String> nameValuePair;
-    
+
     @Override
     public void runInternal(GeogitCLI cli) throws Exception {
 
-    	final GeoGIT geogit = new GeoGIT();
-    	try {
-    		final List<String> status = geogit.command(ConfigOp.class).setGet(get).setGlobal(global).setNameValuePair(nameValuePair).call();
-    		
-    		if (status.size() == 2) {
-    			System.out.println(status.get(0) + " = " + status.get(1));
-    		}
-    	}
-    	catch (ConfigException e) {
-            // These mirror 'git config' status codes.  Some of these are unused, since we don't have regex support yet.
-            switch (e.statusCode) {
-            	case 1:
-            		System.out.println("The config location is invalid");
-            		break;
-            	case 2:
-            		System.out.println("Cannot write to the config");
-            		break;
-            	case 3:
-            		System.out.println("No section or name was provided");
-            		break;
-            	case 4:
-            		System.out.println("The section or key is invalid");
-            		break;
-            	case 5:
-            		System.out.println("Tried to unset an option that does not exist");
-            		break;
-            	case 6:
-            		System.out.println("Tried to unset/set an option for which multiple lines match");
-            		break;
-            	case 7:
-            		System.out.println("Tried to use an invalid regexp");
-            		break;
-            	case 8:
-            		System.out.println("Used --global option without $HOME being properly set");
-            		break;
+        final GeoGIT geogit = new GeoGIT(cli.getPlatform().pwd());
+        try {
+            final List<String> status = geogit.command(ConfigOp.class).setGet(get)
+                    .setGlobal(global).setNameValuePair(nameValuePair).call();
+
+            if (status.size() == 2) {
+                System.out.println(status.get(0) + " = " + status.get(1));
             }
-    	}
+        } catch (ConfigException e) {
+            // These mirror 'git config' status codes. Some of these are unused,
+            // since we don't have regex support yet.
+            switch (e.statusCode) {
+            case INVALID_LOCATION:
+                cli.getConsole().println("The config location is invalid");
+                break;
+            case CANNOT_WRITE:
+                cli.getConsole().println("Cannot write to the config");
+                break;
+            case SECTION_OR_NAME_NOT_PROVIDED:
+                cli.getConsole().println("No section or name was provided");
+                break;
+            case SECTION_OR_KEY_INVALID:
+                cli.getConsole().println("The section or key is invalid");
+                break;
+            case OPTION_DOES_N0T_EXIST:
+                cli.getConsole().println("Tried to unset an option that does not exist");
+                break;
+            case MULTIPLE_OPTIONS_MATCH:
+                cli.getConsole().println(
+                        "Tried to unset/set an option for which multiple lines match");
+                break;
+            case INVALID_REGEXP:
+                cli.getConsole().println("Tried to use an invalid regexp");
+                break;
+            case USERHOME_NOT_SET:
+                cli.getConsole().println("Used --global option without $HOME being properly set");
+                break;
+            }
+        }
     }
 
 }
--- src/core/src/main/java/org/geogit/storage/ConfigDatabase.java
+++ src/core/src/main/java/org/geogit/storage/ConfigDatabase.java
@@ -1,21 +1,29 @@
 package org.geogit.storage;
 
-
 public interface ConfigDatabase {
+    public enum StatusCode {
+        INVALID_LOCATION, CANNOT_WRITE, SECTION_OR_NAME_NOT_PROVIDED, SECTION_OR_KEY_INVALID, OPTION_DOES_N0T_EXIST, MULTIPLE_OPTIONS_MATCH, INVALID_REGEXP, USERHOME_NOT_SET
+    }
 
-	@SuppressWarnings("serial")
-	public class ConfigException extends Exception {
-		public int statusCode;
-		
-		public ConfigException(int statusCode) {
-			this.statusCode = statusCode;
-		}
-	}
-	
-	public String get(String key) throws ConfigException;
-	public String getGlobal(String key) throws ConfigException;
+    @SuppressWarnings("serial")
+    public class ConfigException extends Exception {
+        public StatusCode statusCode;
 
-	public void put(String key, Object value) throws ConfigException;
-	public void putGlobal(String key, Object value) throws ConfigException;
-	
+        public ConfigException(StatusCode statusCode) {
+            this.statusCode = statusCode;
+        }
+    }
+
+    public String get(String key) throws ConfigException;
+
+    public String getGlobal(String key) throws ConfigException;
+
+    public <T> T get(String key, Class<T> c) throws ConfigException;
+
+    public <T> T getGlobal(String key, Class<T> c) throws ConfigException;
+
+    public void put(String key, Object value) throws ConfigException;
+
+    public void putGlobal(String key, Object value) throws ConfigException;
+
 }
